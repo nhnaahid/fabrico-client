ab1fa028cfa7b9ff346a0daa88c00f25ca0ac874
nhnaahid <nhnahid.iu@gmail.com>
2024-08-15 09:26:33 +0600

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            }
+        ]
+    },
+]);
+
+export default router;
