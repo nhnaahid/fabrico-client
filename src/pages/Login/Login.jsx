8427688312bf436b69db03394f9b2da48dab0959
nhnaahid <nhnahid.iu@gmail.com>
2024-08-15 17:28:07 +0600

    add: login, signup page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom";
+import SmallButton from "../../components/Buttons/SmallButton";
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="w-full md:w-3/5 border py-5 mx-auto flex items-center justify-center">
+            <form className="grid w-4/5 mx-auto gap-2">
+                <h1 className="font-bold text-xl">Account Login</h1>
+                <div className="divider"></div>
+
+                <label className="text-start">Email</label>
+                <input className="border  rounded-lg p-2" type="email" name="email" id="" placeholder="email" required />
+
+                <label className="text-start">Password</label>
+                <input className="border  rounded-lg p-2" type="password" name="password" id="" placeholder="password" required />
+
+                <input className="btn btn-sm border-emerald-600 mt-5" type="submit" value="Login" />
+
+                <p className="mt-3">New User? <Link className="font-bold" to="/signup">Sign Up</Link></p>
+
+                <div className="divider ">Or</div>
+
+                <SmallButton name="Login with Google"></SmallButton>
+
+            </form>
         </div>
     );
 };
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,42 @@
+import { Link } from "react-router-dom";
+import SmallButton from "../../components/Buttons/SmallButton";
 
 const SignUp = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photo = form.photo.value;
+        console.log(name, email, photo);
+    }
     return (
-        <div>
-            <h1>SignUp</h1>
+        <div className="w-full md:w-3/5 py-5 border mx-auto flex items-center justify-center">
+            <form onSubmit={handleSubmit} className="grid w-4/5 mx-auto gap-2">
+                <h1 className="font-bold text-xl">Account Registration</h1>
+                <div className="divider"></div>
+
+                <label className="text-start">Name</label>
+                <input className="border rounded-lg p-2" type="text" name="name" id="" placeholder="name" required />
+
+                <label className="text-start">Email</label>
+                <input className="border rounded-lg p-2" type="email" name="email" id="" placeholder="email" required />
+
+                <label className="text-start">Photo URL</label>
+                <input className="border rounded-lg p-2" type="text" name="photo" id="" placeholder="photo url" required />
+
+                <label className="text-start">Password</label>
+                <input className="border rounded-lg p-2" type="password" name="password" id="" placeholder="password" required />
+
+                <input className="btn btn-sm mt-5 border-emerald-500" type="submit" value="Sign Up" />
+
+                <p className="mt-3">Old User? <Link className="font-bold" to="/login">Login</Link></p>
+
+                <div className="divider">Or</div>
+
+                <SmallButton name="Sign up with Google"></SmallButton>
+
+            </form>
         </div>
     );
 };
